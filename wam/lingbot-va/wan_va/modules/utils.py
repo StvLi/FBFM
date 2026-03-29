0abdfdc977851adb0bab8e40a305b1bebf29b62d
littleZ05 <18511293290@163.com>
2026-03-29 19:16:19 +0800

    debug: add optional vae module shape hooks

--- wam/lingbot-va/wan_va/modules/utils.py
+++ wam/lingbot-va/wan_va/modules/utils.py
@@ -1,5 +1,6 @@
 # Copyright 2024-2025 The Robbyant Team Authors. All rights reserved.
 import logging
+import os
 
 import torch
 from diffusers import AutoencoderKLWan
@@ -87,14 +88,46 @@ class WanVAEStreamingWrapper:
     def clear_cache(self):
         self.feat_cache = [None] * self.enc_conv_num
 
+    def _register_debug_hooks(self):
+        handles = []
+
+        def should_trace(name, module):
+            lname = name.lower()
+            return (
+                "shortcut" in lname
+                or "downsample" in lname
+                or "time_conv" in lname
+                or module.__class__.__name__ == "WanCausalConv3d"
+            )
+
+        def hook(name):
+            def _hook(module, inputs, output):
+                in_shape = None
+                if inputs and isinstance(inputs[0], torch.Tensor):
+                    in_shape = tuple(inputs[0].shape)
+                out_shape = tuple(output.shape) if isinstance(output, torch.Tensor) else type(output).__name__
+                logger.info("VAE hook %s: in=%s out=%s", name, in_shape, out_shape)
+            return _hook
+
+        for name, module in self.encoder.named_modules():
+            if should_trace(name, module):
+                handles.append(module.register_forward_hook(hook(name)))
+        return handles
+
     def encode_chunk(self, x_chunk):
         if hasattr(self.vae.config,
                    "patch_size") and self.vae.config.patch_size is not None:
             x_chunk = patchify(x_chunk, self.vae.config.patch_size)
         logger.info("encode_chunk input shape=%s", tuple(x_chunk.shape))
         feat_idx = [0]
-        out = self.encoder(x_chunk,
-                           feat_cache=self.feat_cache,
-                           feat_idx=feat_idx)
+        debug_shapes = os.getenv("WAN_VAE_DEBUG_SHAPES", "0") == "1"
+        handles = self._register_debug_hooks() if debug_shapes else []
+        try:
+            out = self.encoder(x_chunk,
+                               feat_cache=self.feat_cache,
+                               feat_idx=feat_idx)
+        finally:
+            for handle in handles:
+                handle.remove()
         enc = self.quant_conv(out)
         return enc
